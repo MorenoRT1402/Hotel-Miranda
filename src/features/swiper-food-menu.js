b380cf7ab2ce17c984ada71c4dc267d59800ebdc
MorenoRT1402 <morenort1402@gmail.com>
2024-08-28 15:14:15 +0200

    feat: home food menu (mobile)

---
+++ src/features/swiper-food-menu.js
@@ -0,0 +1,9 @@
+const swiperFoodMenu = new Swiper('.swiper-food-menu', {
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
