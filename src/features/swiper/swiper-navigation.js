bcbbee755435732c22479cece33b1e309582019a
Carlos Moreno <morenort1402@gmail.com>
2024-11-15 15:47:07 +0100

    FEAT home food desktop layout

--- src/features/swiper/swiper-navigation.js
+++ src/features/swiper/swiper-navigation.js
@@ -16,3 +16,22 @@ const swiperNavigation = new Swiper('.swiper-navigation', {
         },
       },
 });
+
+const swiperNavigationFood = new Swiper('.swiper-navigation-food', {
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: false,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            loop: false,
+        },
+      },
+});
