63af85c1c091cf60a2f5491717ef9c8a9d2b07c9
Carlos Moreno <morenort1402@gmail.com>
2024-12-09 15:36:35 +0100

    FEAT : about page desktop layout

--- src/features/swiper/swiper-pagination.js
+++ src/features/swiper/swiper-pagination.js
@@ -27,3 +27,30 @@ const swiperPagination = new Swiper('.swiper-pag', {
         },
     },
 });
+
+const swiperPaginationTwoVisibles = new Swiper('.swiper-pag--dt--two-visibles', {
+    direction: 'horizontal',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            loop: false,
+            spaceBetween: 20,
+            pagination: false,
+        },
+
+        0: {
+            slidesPerView: 1,
+            grid: {
+                rows: 1,
+            },
+            slidesPerGroup: 1,
+            loop: true,
+        },
+    },
+});
